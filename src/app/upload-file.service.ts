16c98498fc46e083029bd8316de11bce22906648
Paulo Sergio de Matos <paulo.matos@medral.com.br>
2021-04-30 17:18:04 -0300

    aula 137 consumindo o backend criado em node

---
+++ src/app/upload-file.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UploadFileService {
+
+  constructor(private http: HttpClient) { }
+
+  upload(files: Set<File>, url: string): any {
+    // para enviar os arquivos no corpo do request, utiliza-se a classe FormData
+    const formData = new FormData();
+    files.forEach(file => formData.append('file', file, file.name));
+    // utilizando um request customizado. Dieferente do padrao Get, Post, Put, Delete
+    const request = new HttpRequest('POST', url, formData);
+    return this.http.request(request);
+
+    // da forma convencional seria apenas:
+    // return this.http.post(url, formData);
+  }
+}
--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,3 +1,4 @@
+import { UploadFileService } from './../../upload-file.service';
 import { Component, OnInit } from '@angular/core';
 import { InputFiles } from 'typescript';
 
@@ -8,7 +9,10 @@ import { InputFiles } from 'typescript';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  // o tipo Set faz automaticamente o expurgo de dados duplicados... caso o usuario selecione o mesmo arquivo duas vezes
+  files: Set<File> | undefined;
+
+  constructor(private service: UploadFileService) { }
 
   ngOnInit(): void {
   }
@@ -17,12 +21,24 @@ export class UploadFileComponent implements OnInit {
     const selectedFiles = (event.currentTarget as HTMLInputElement).files;
     const lbl = document.getElementById('customFileLabel');
     if (lbl && selectedFiles) {
+      this.files = new Set<File>();
       const fileNames = [];
       for (let i = 0; i < selectedFiles.length; i++) {
         const element = selectedFiles.item(i);
         fileNames.push(element?.name);
+        if (element) {
+          this.files.add(element);
+        }
       }
       lbl.innerHTML = fileNames.join(', ');
     }
+  }
+
+  onUpload(): any {
+    if (this.files && this.files.size > 0) {
+      this.service.upload(this.files, 'http://localhost:8000/upload')
+        .subscribe((response: any) => console.log('Upload concluido'));
+    }
+    // TODO: fazer o unsubscribe
   }
 }
