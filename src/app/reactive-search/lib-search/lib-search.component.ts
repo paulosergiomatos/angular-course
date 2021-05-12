c0cee46fbacaecc3b2968151c6629e73b29668de
Paulo Sergio de Matos <paulo.matos@medral.com.br>
2021-05-12 16:32:59 -0300

    aula 143 - passando parametros para a url

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-lib-search',
@@ -9,14 +13,42 @@ import { FormControl } from '@angular/forms';
 export class LibSearchComponent implements OnInit {
 
   queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$ = new Observable<any>();
+  total = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   onSearch(): any {
-    console.log(this.queryField.value);
-  }
+    const fields = 'name,description,version,homepage';
+    let value = this.queryField.value;
+    // tslint:disable-next-line: no-conditional-assignment
+    if (value && (value = value.trim()) !== '') {
+      // ? a interrogacao é para informar a passagem de parametros
+      // & o  ecomercial é para os demais parametros
+      // this.results$ = this.http.get(`${this.SEARCH_URL}?fields=${fields}&search=${this.queryField.value}`)
 
+      // utilizando o angular params... o proprio angular cria a url
+      // com as interrogações e ecomerciais conforme o exemplo acima
+      const params = {
+        search: value,
+        fields
+      };
+
+      let params2 = new HttpParams(); // similar ao anterior porem de forma mais dinamica tambem é do angular
+      params2 = params2.append('search', value);
+      params2 = params2.append('fields', fields);
+      // params2 = params2.append() ...
+
+
+      this.results$ = this.http.get(this.SEARCH_URL, { params: params2 })
+        .pipe(
+          tap((res: any) => this.total = res.total),
+          map(res => res.results)
+        );
+    }
+  }
 }
